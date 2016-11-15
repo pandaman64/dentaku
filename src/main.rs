855abd4e68e94ecb1353ccba5b5199d7ccddbfa6
pandaman64 <kointosudesuyo@infoseek.jp>
2016-11-15 16:40:11 +0900

    Use Result<T,E>.

--- src/main.rs
+++ src/main.rs
@@ -11,36 +11,25 @@ enum Expr{
 }
 
 #[derive(Debug)]
-enum ParseResult<T,Iter: Iterator<Item=char>>{
-    // parsed expression, remaining string
-    Success(T,Box<Iter>),
-    Fail
-}
+struct ParseSuccess<T,Iter: Iterator<Item=char>>(T,Box<Iter>);
 
-impl<T,Iter: Iterator<Item=char>> ParseResult<T,Iter>{
-    fn successed(&self) -> bool{
-        match self{
-            &ParseResult::Success(_,_) => true,
-            &ParseResult::Fail => false
-        }
-    }
-}
+type ParseResult<T,Iter> = Result<ParseSuccess<T,Iter>,()>;
 
 //とりあえず一文字
 fn num<Iter: Iterator<Item=char>>(mut iter: Box<Iter>) -> ParseResult<Expr,Iter>{
     println!("num");
     match iter.next(){
-        Some('0') => ParseResult::Success(Expr::Number(0),iter),
-        Some('1') => ParseResult::Success(Expr::Number(1),iter),
-        Some('2') => ParseResult::Success(Expr::Number(2),iter),
-        Some('3') => ParseResult::Success(Expr::Number(3),iter),
-        Some('4') => ParseResult::Success(Expr::Number(4),iter),
-        Some('5') => ParseResult::Success(Expr::Number(5),iter),
-        Some('6') => ParseResult::Success(Expr::Number(6),iter),
-        Some('7') => ParseResult::Success(Expr::Number(7),iter),
-        Some('8') => ParseResult::Success(Expr::Number(8),iter),
-        Some('9') => ParseResult::Success(Expr::Number(9),iter),
-        _ => ParseResult::Fail
+        Some('0') => Result::Ok(ParseSuccess(Expr::Number(0),iter)),
+        Some('1') => Result::Ok(ParseSuccess(Expr::Number(1),iter)),
+        Some('2') => Result::Ok(ParseSuccess(Expr::Number(2),iter)),
+        Some('3') => Result::Ok(ParseSuccess(Expr::Number(3),iter)),
+        Some('4') => Result::Ok(ParseSuccess(Expr::Number(4),iter)),
+        Some('5') => Result::Ok(ParseSuccess(Expr::Number(5),iter)),
+        Some('6') => Result::Ok(ParseSuccess(Expr::Number(6),iter)),
+        Some('7') => Result::Ok(ParseSuccess(Expr::Number(7),iter)),
+        Some('8') => Result::Ok(ParseSuccess(Expr::Number(8),iter)),
+        Some('9') => Result::Ok(ParseSuccess(Expr::Number(9),iter)),
+        _ => Result::Err(())
     }
 }
 
@@ -51,7 +40,7 @@ fn primitive<Iter: Iterator<Item=char>>(mut iter: Box<Iter>) -> ParseResult<Expr
 /*
 fn parseChar<Iter: Iterator<char>>(c: char,iter: &mut Iter) -> ParseResult<char,Iter>{
     match iter.next(){
-        c => ParseResult::Success(c,iter),
+        c => Result::Ok(c,iter),
         _ => Fail
     }
 }
@@ -59,30 +48,20 @@ fn parseChar<Iter: Iterator<char>>(c: char,iter: &mut Iter) -> ParseResult<char,
 
 fn multitive<Iter: Iterator<Item=char> + Clone>(mut iter: Box<Iter>) -> ParseResult<Expr,Iter>{
     println!("mult");
-    if let ParseResult::Success(lhs,iter) = primitive(iter){
+    primitive(iter).and_then(|ParseSuccess(lhs,iter)|{
         let mut clone = iter.clone();
-        let op = match clone.next(){
-            Some('*') => ParseResult::Success('*',clone),
-            _ => ParseResult::Fail
-        };
-        if let ParseResult::Success(op,iter) = op{
-            if let ParseResult::Success(rhs,iter) = primitive(iter){
-                ParseResult::Success(Expr::Multiply(Box::new(lhs),Box::new(rhs)),iter)
-            }
-            else{
-                ParseResult::Fail
-            }     
+        match clone.next(){
+            Some('*') => primitive(clone).map(|ParseSuccess(rhs,iter)|{
+                ParseSuccess(Expr::Multiply(Box::new(lhs),Box::new(rhs)),iter)
+            }),
+            Some(_) => Result::Err(()),
+            _ => Result::Ok(ParseSuccess(lhs,iter))
         }
-        else{
-            ParseResult::Success(lhs,iter)
-        }
-    }
-    else{
-        ParseResult::Fail
-    }
+    })
 }
 
 fn main() {
-    let input = "1*2";
+    let input = "1";
     println!("{:?}",multitive(Box::new(input.chars())));
+    println!("{:?}",multitive(Box::new("1*2".chars())));
 }
